364dbfc2b72669e7df852b2e154bdbd176828a55
Ovanes Budakyan <obudakyan@ovanes.tuxedo>
2017-03-30 12:12:40 +0100

    Added Update_Single_Tables_Using_Joints

--- MySQL/Update/Update_Single_Tables_Using_Joints.sql
+++ MySQL/Update/Update_Single_Tables_Using_Joints.sql
@@ -4,8 +4,8 @@
  */
 
     UPDATE `my_table` AS `mt`
-    INNER JOIN `table_one` AS `d` ON(`ta`.`val`=`mt`.`val`)
-    INNER JOIN `table_two` AS `pa` ON(`tb`.`PersonID` = `ta`.`val`)
-    INNER JOIN `table_tree` AS `a` ON(`tc`.`val` = `tb`.`val`)
+    INNER JOIN `table_one` AS `ta` ON(`ta`.`val`=`mt`.`val`)
+    INNER JOIN `table_two` AS `tb` ON(`tb`.`PersonID` = `ta`.`val`)
+    INNER JOIN `table_tree` AS `tc` ON(`tc`.`val` = `tb`.`val`)
     SET `mt`.`val`= 'value'
     WHERE `mt`.`val` = 'value' AND `tc`.`val` = 'value' AND `tc`.`val` = 'value';
