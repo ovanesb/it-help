2a6c6699844591fa86dc5979811dd975854779ba
Ovanes Budakyan <obudakyan@ovanes.tuxedo>
2017-03-31 11:05:49 +0100

    Added CreateStoredProcedure MSSQL

---
+++ MSSQL/Stored_Procedures/CreateStoredProcedure.sql
@@ -0,0 +1,21 @@
+/**
+ * Author:  ovanes
+ * Created: March 31, 2017
+ */
+
+USE [db]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [db].[myStoredProcedure] 	
+AS
+BEGIN
+	
+    -- My code in here......
+
+END
